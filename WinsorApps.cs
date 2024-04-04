36df5729174ef438d3c909e49e0f506bd7a1889d
Saratu <saratu.waya@winsor.edu>
2024-04-04 11:31:46 -0400

    rpwigh[wpGH

---
+++ WinsorApps.cs
@@ -0,0 +1,14 @@
+namespace CS_First_HTTP_Client;
+
+/// <summary>
+/// login object used with the Winsor Apps API
+/// </summary>
+/// <param name="email"></param>
+/// <param name="password"></param>
+/// what are the specifics of the problem (if applicable)
+/// sometimes this is just a message
+public readonly record struct Login(string email, string password);
+
+public readonly record struct ErrorResponse(string type string error);
+
+public readonly record struct AuthResponse(string userID, string jwt, DateTime expires, string refreshToken);
